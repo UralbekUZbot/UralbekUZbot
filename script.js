be4737e518d4ed785dc65a380f022fd6a12f2ab7
UralbekUZbot <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-14 17:07:59 +0500

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Uralbek Tursunov | Script | @UralbekUZbot | v1.0.0 (14.06.2024)
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
