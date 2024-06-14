561709e63640363d674610c767a395728e2fdaaf
UralbekUZbot <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-14 17:18:22 +0500

    update script.js

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ function startTime() {
     let h = today.getHours();
     let m = today.getMinutes();
     let s = today.getSeconds();
-    
+    h = checkTime(h);
     m = checkTime(m);
     s = checkTime(s);
+    
+    document.get
 }
