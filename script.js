ba94b348848abf3b80d993a33248134931aa9649
UralbekUZbot <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-14 17:17:12 +0500

    update script.js

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ function startTime() {
     const today = new Date();
     let h = today.getHours();
     let m = today.getMinutes();
-    let s = today.getSeconds()
+    let s = today.getSeconds();
+    h 
 }
