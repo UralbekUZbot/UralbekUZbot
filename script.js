2efebded4a23ecc0db812afbb5473616fcc20429
UralbekUZbot <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-14 17:12:42 +0500

    update script.js

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 // Uralbek Tursunov | Script | @UralbekUZbot | v1.0.0 (14.06.2024)
-function startTime() {
-  const today = new Date();
-  let h = today.getHours();
-  let m = today.getMinutes();
-  let s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  
-  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
-  setTimeout(startTime, 1000);
-}
-
-function checkTime(i) {
-  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-  return i;
-}
+func
