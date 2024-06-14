39c5c86eca74f8c440ad6a254a0faa072cca9bde
UralbekUZbot <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-15 00:09:54 +0500

    update script.js

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ function checkTime(i) {
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
