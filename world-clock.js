cfb3b8f43bdf509ea0891706ee8c8f5f65b404ef
UralbekUZbot <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-15 14:49:46 +0500

    update world-clock.js

--- world-clock.js
+++ world-clock.js
@@ -1,13 +1,1 @@
 // Uralbek Tursunov | @UralbekUZbot | v1.4.7 (140624)
-var data=[
-    {
-        coutry:"NEW YORK",
-        timeZone:"America/New_York",
-    },
-    {
-        coutry:"LONDON",
-        timeZone:"Europe/London",
-    }
-]
-
-option={day:"2-digit",month:"short",year:"numeric",hour12:false,hour:"numeric",minute:"2-digit",second:"2-digit"
