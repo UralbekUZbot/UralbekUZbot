f29c72e60f16e2a865f2c0b6c8fa36e1bc218db9
UralbekUZbot <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-15 05:30:36 +0500

    update script.js

--- script.js
+++ script.js
@@ -16,11 +16,10 @@ function checkTime(i) {
   return i;
 }
 
-function myFunction() {
-  var x = document.getElementById("Demo");
-  if (x.className.indexOf("w3-show") == -1) { 
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
 }
