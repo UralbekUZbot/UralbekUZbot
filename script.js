b9c03a60bad3d76f6691749526df609d5cfc835a
UralbekUZbot <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-17 19:27:15 +0500

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.getElementById("demo").innerHTML = "Hello World!";
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+    myFunction(this);
+    }
+};
+xhttp.open("GET", "books.xml", true);
+xhttp.send();
+
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    document.getElementById("demo").innerHTML =
+    xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+}
