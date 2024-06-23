c0be6933b69fdbf3407205addd24e28739779412
Uralbek Tursunov <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-23 16:39:02 +0500

    update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    
+    document.getElementById('soat').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
