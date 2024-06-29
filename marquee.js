f911b035bca2357c9d170185d2927d7c4cbd3af9
Uralbek Tursunov <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-29 11:03:52 +0500

    update marquee.js

--- marquee.js
+++ marquee.js
@@ -4,7 +4,7 @@ xhttp.onreadystatechange = function() {
         myFunction(this);
     }
 };
-xhttp.open("GET", "books.xml", true);
+xhttp.open("GET", "marquee.xml", true);
 xhttp.send();
 
 function myFunction(xml) {
