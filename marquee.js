4faca19d82c70f373aa8c18df9479ca3f94c0913
Uralbek Tursunov <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-29 11:23:22 +0500

    update marquee.js

--- marquee.js
+++ marquee.js
@@ -1,20 +1,2 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
-    }
-};
-xhttp.open("GET", "https://UralbekUZbot.netlify.app/marquee.xml", true);
-xhttp.send();
+const letters = new Set(["bugun havo juda issiq","issiqda koʻchada yurmang","11 dan 15 gacha harorat 45 C°"]);
 
-function myFunction(xml) {
-    var xmlDoc = xml.responseXML;
-    document.getElementById("marquee").innerHTML =
-    xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
-}
-
-function myFunction(xml) {
-    var xmlDoc = xml.responseXML;
-    document.getElementById("marquee-valyuta").innerHTML =
-    xmlDoc.getElementsByTagName("valyuta")[2].childNodes[2].nodeValue;
-}
