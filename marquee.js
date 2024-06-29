9d6abb640b9f3a66d1bfb2dabf8d651e613b9de6
Uralbek Tursunov <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-29 12:01:03 +0500

    update marquee.js

---
+++ marquee.js
@@ -0,0 +1,14 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+    }
+};
+xhttp.open("GET", "books.xml", true);
+xhttp.send();
+
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    document.getElementById("demo").innerHTML =
+    xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+}
