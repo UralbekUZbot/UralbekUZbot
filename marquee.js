af8626e599d0b9ccdc36a744a860d4bfe0278a55
Uralbek Tursunov <162903053+UralbekUZbot@users.noreply.github.com>
2024-06-29 11:55:04 +0500

    update marquee.js

--- marquee.js
+++ marquee.js
@@ -1,8 +1,1 @@
-const letters = new Set(["bugun havo juda issiq","issiqda koʻchada yurmang","11 dan 15 gacha harorat 45 C°"]);
-
-let text = "";
-for (const x of letters) {
-  text += x + "<br>";
-}
-
-document.getElementById("marquee").innerHTML = text;
+txt = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
